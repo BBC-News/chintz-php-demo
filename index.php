4fa4b3c74198f763ffc1cde4fb43cddd3a96e1b7
pgchamberlin <pgchamberlin@googlemail.com>
2015-01-13 18:17:07 +0000

    New and improved

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-// An organism we want to render?
-$elementName = 'elementA';
+$mustachePartialLoader = new Chintz_Templater_Mustache_FileSystemAliasLoader();
+$mustache = new Mustache_Engine(
+    array(
+        'partials_loader' => $mustachePartialLoader
+    )
+);
 
 $chintzParams = array(
-    'chintz-base-path' => dirname(__FILE__) . '/vendor/pgchamberlin/chintz'
+    'chintz-base-path' => dirname(__FILE__) . '/vendor/pgchamberlin/chintz',
+    'handlers' => array(
+        'css' => new Chintz_Handler_CSS()
+    ),
+    'templater' => new Chintz_Templater_Mustache($mustache, $mustachePartialLoader)
 );
 
 $chintzParser = new Chintz_Parser($chintzParams);
+
+$elementName = 'elementA';
+
 $chintzParser->prepare($elementName);
 
 $data = array(
@@ -33,7 +44,7 @@ $data = array(
 <html>
 <head>
     <style type="text/css">
-        <?= $chintzParser->rawCSS() ?>
+        <?= $chintzParser->getDependencies('css') ?>
     </style>
 </head>
 <body>
